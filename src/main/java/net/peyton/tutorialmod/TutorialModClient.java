23930f26649e4f06d66dba4254fb9235f60de887
peyton-lackner <peyton.lackner@gmail.com>
2022-12-27 17:56:13 -0600

    Initial commit

---
+++ src/main/java/net/peyton/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.peyton.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
