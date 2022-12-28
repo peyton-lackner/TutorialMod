2db36723371d81f0eb06e71ab369502442c271e3
peyton-lackner <peyton.lackner@gmail.com>
2022-12-28 10:39:06 -0600

    Added tanzanite group and created 5 new blocks (block of tanzanite, tanzanite ore and deepslate, netherrack, and endstone variants of ore).

--- src/main/java/net/peyton/tutorialmod/TutorialMod.java
+++ src/main/java/net/peyton/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.peyton.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.peyton.tutorialmod.block.ModBlocks;
 import net.peyton.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.RegisterModItems();
+		ModBlocks.RegisterModBlocks();
 	}
 }
--- src/main/java/net/peyton/tutorialmod/item/ModItems.java
+++ src/main/java/net/peyton/tutorialmod/item/ModItems.java
@@ -3,7 +3,6 @@ package net.peyton.tutorialmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,7 +13,7 @@ public class ModItems {
     public static final Item RAW_TANZANITE = registerItem(
             "raw_tanzanite",
             new Item(new Item.Settings()),
-            ItemGroups.NATURAL
+            ModItemGroup.TANZANITE
     );
 
     public static final Item TANZANITE = registerItem(
