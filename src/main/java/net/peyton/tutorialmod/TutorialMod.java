253bd12ae91e777acfd7292c05c52fcc96c69a50
peyton-lackner <peyton.lackner@gmail.com>
2022-12-27 18:26:26 -0600

    Added Tanzanite

--- src/main/java/net/peyton/tutorialmod/TutorialMod.java
+++ src/main/java/net/peyton/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.peyton.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.peyton.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		ModItems.RegisterModItems();
 	}
 }
